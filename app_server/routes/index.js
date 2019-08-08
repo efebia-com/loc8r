cc5b0f72f4c51c6402f6d547b3acb854f90070d4
Lorenzo Fiore <lorenzo@efebia.com>
2019-08-08 11:58:20 +0200

    Add view tempolates

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-var express = require('express');
-var router = express.Router();
-const ctrlMain = require('../controllers/main');
-
-/* GET homepage. */
-router.get('/', ctrlMain.index);
-=======
 const express = require('express');
 const router = express.Router();
 const ctrlLocations = require('../controllers/locations');
@@ -16,6 +8,5 @@ router.get('/location', ctrlLocations.locationInfo);
 router.get('/location/review/new', ctrlLocations.addReview);
 
 router.get('/about', ctrlOthers.about);
->>>>>>> 33fc5bd6a9b1ed6a7cf15a9bfe302e0b85c77a67
 
 module.exports = router;
